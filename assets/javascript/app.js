da851d5450c1c7778d6e98b8ad9031e2f53ec38d
FightingSprrow <kbauertx@gmail.com>
2018-11-02 14:19:01 -0500

    for loop created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
 // NYT API App
 
 $("searchButton").on("click", function() {
+
     var dataSearch = $(this).attr("data-search");
     var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
 $.ajax({
   url: url,
   method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+    }).done(function(result) {
+    console.log(result);
+    }).fail(function(err) {
+    throw err;
+}).then(function(response) {
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+        var articleDiv = $("<div>");
+        // headline.main
+        // byline.original
+        var headline = $("<h3>").text(response[i].headline.main);
+        var author = $("<h5>").text(response[i].byline.original);
+
+        articleDiv.append(headline);
+        articleDiv.append(author);
+        $(".articlesList").append(articleDiv);
+    }
+})
 });
-
-
