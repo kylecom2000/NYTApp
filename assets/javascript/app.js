d7891951c17d9cfa6618a40710dd52051e5145bf
FightingSprrow <kbauertx@gmail.com>
2018-11-02 13:33:26 -0500

    ajax added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,12 @@
 // NYT API App
 
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+$.ajax({
+  url: url,
+  method: 'GET',
+}).done(function(result) {
+  console.log(result);
+}).fail(function(err) {
+  throw err;
+});
+
